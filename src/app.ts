5ab96910f4b7074e56d61b556d5470de0b796fd2
Mukut Chakrabarty <mukutckb1@gmail.com>
2023-05-28 21:40:48 +0600

    created .env cors and body parser

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response} from "express";
+import cors from 'cors'
+const app: Application = express();
+const port : Number = 3000;
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended : true}) )
 
-
-const app : Application = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+app.get("/", (req : Request, res : Response) => {
+  res.send("Hello World!");
+});
 
 export default app;
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,6 +5,4 @@ dotenv.config({path : path.join(process.cwd() , '.env')})
 export default {
     port : process.env.PORT,
     db_url : process.env.DB_URL,
-    db_user : process.env.DB_USER,
-    db_password : process.env.DB_PASS,
 }
