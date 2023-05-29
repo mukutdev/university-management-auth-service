c200bb6db172dc48d06702788d426721e930d74d
Mukut Chakrabarty <mukutckb1@gmail.com>
2023-05-29 11:56:02 +0600

    lint-stage

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
-        app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
